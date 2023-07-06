a0880e143ed55536a8784375034e50640e65ae55
sang won Yoo (유상원) <rongleader@naver.com>
2023-07-06 17:24:09 +0900

    계산기 프로젝트

---
+++ Calculator.cpp
@@ -0,0 +1,22 @@
+#include "Calculator.h"
+
+double Calculator::Calculate(double x, char oper, double y)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+        break;
+    case '-':
+        return x - y;
+        break;
+    case '*':
+        return x * y;
+        break;
+    case '/':
+        return x / y;
+        break;
+    default:
+        return 0.0;
+    }
+}
